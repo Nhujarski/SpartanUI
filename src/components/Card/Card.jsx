7cad46c7ce56600e90eca322d85a27c028cf0a55
Nhujarski <52231438+Nhujarski@users.noreply.github.com>
2022-08-24 15:46:59 -0700

    add more css and controls in story

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,23 +1,22 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './card.css';
-import Wrapper from '../Wrapper/Wrapper';
 
-const Card = ({ header, body, footer }) => {
+const Card = ({ header, body, footer, size }) => {
   return (
-    <Wrapper>
-      <div className='sui-card'>
-        <header className='sui-card-header-container'>
-          <h1 className='sui-card-header'>{header}</h1>
-        </header>
-        <main className='sui-card-body-container'>
-          <p className='sui-card-body'>{body}</p>
-        </main>
+    <div className={['sui-card', `sui-card-${size}`].join(' ')}>
+      <header className='sui-card-header-container'>
+        <h1 className='sui-card-header'>{header}</h1>
+      </header>
+      <main className='sui-card-body-container'>
+        <img className='sui-card-body' src={body} />
+      </main>
+      {footer ? (
         <footer className='sui-card-footer-container'>
-          <p className='sui-card-footer'>{footer}</p>
+          <div className='sui-card-footer'>{footer}</div>
         </footer>
-      </div>
-    </Wrapper>
+      ) : null}
+    </div>
   );
 };
 
--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './wrapper.css';
 
-const Wrapper = ({ size, bgColor, children }) => {
+const Wrapper = ({ bgColor, children }) => {
   let approvedBgColors = ['light', 'dark'];
   return (
     <div
       className={[
         'sui-wrapper',
-        `sui-wrapper-${size}`,
+        'sui-wrapper-large',
         `sui-wrapper-${approvedBgColors.includes(bgColor) ? bgColor : 'light'}`,
       ].join(' ')}>
       {children}
